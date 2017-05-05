553c9ed1e74b30c9e9ccdb29f6918d5c0b4b1550
Zakir <zakirshikhli@gmail.com>
2017-05-05 10:48:04 +0400

    Add files via upload

---
+++ Unity_Standalone_Portrait_Fix+AntiFastSpeed.cs
@@ -0,0 +1,30 @@
+/// <summary>
+///
+///   ___Made by Zakir Shikhli___
+///   
+/// https://www.facebook.com/DarkAndLightDevelopmentStudio
+/// https://www.facebook.com/zakirshikhli
+/// 
+/// ____Free for use and share____
+/// 
+/// </summary>
+/// 
+
+
+void Awake()
+{
+#if UNITY_EDITOR || UNITY_STANDALONE
+
+        Screen.SetResolution
+         (       
+         Screen.resolutions[Screen.resolutions.Length - 1].height*9/16,
+         Screen.resolutions[Screen.resolutions.Length - 1].height - Screen.resolutions[Screen.resolutions.Length - 1].height/10,
+         false
+         );
+
+        Resolution res = Screen.currentResolution;
+        if (res.refreshRate == 60)  QualitySettings.vSyncCount = 1;
+        if (res.refreshRate == 120) QualitySettings.vSyncCount = 2;
+      
+#endif
+}
